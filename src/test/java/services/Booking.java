858a9bf93ce0de6d70ff34c105cda89c94a39467
caglarozturk-getir <90332473+caglarozturk-getir@users.noreply.github.com>
2022-02-01 12:52:39 +0300

    Initial commit

---
+++ src/test/java/services/Booking.java
@@ -0,0 +1,148 @@
+package services;
+
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static io.restassured.RestAssured.given;
+
+public class Booking {
+
+    String token;
+    String createBookingID;
+
+    @BeforeTest
+    public void createToken(){
+        String postData="{\n" +
+                "    \"username\" : \"admin\",\n" +
+                "    \"password\" : \"password123\"\n" +
+                "}";
+
+        Response response = given()
+                .log().all()
+                .header("Content-Type","application/json")
+                .body(postData)
+                .when()
+                .post("https://restful-booker.herokuapp.com/auth")
+                .then().statusCode(200)
+                .log().all().extract().response();
+        token=response.jsonPath().getString("token");
+    }
+
+
+    @Test(priority = 1)
+    public void PostCreateBooking(){
+        String requestBody="{\n" +
+                "    \"firstname\" : \"Jim\",\n" +
+                "    \"lastname\" : \"Brown\",\n" +
+                "    \"totalprice\" : 111,\n" +
+                "    \"depositpaid\" : true,\n" +
+                "    \"bookingdates\" : {\n" +
+                "        \"checkin\" : \"2018-01-01\",\n" +
+                "        \"checkout\" : \"2019-01-01\"\n" +
+                "    },\n" +
+                "    \"additionalneeds\" : \"Breakfast\"\n" +
+                "}";
+
+        Response requestID = given()
+                .contentType(ContentType.JSON)
+                .header("Content-Type","Application/json")
+                .body(requestBody)
+                .when()
+                .post("https://restful-booker.herokuapp.com/booking")
+                .then().statusCode(200)
+                .log().all().extract().response();
+
+        createBookingID= requestID.jsonPath().getString("bookingid");
+    }
+
+    @Test(priority = 2)
+    public void getBooking (){
+        given()
+                .log().all()
+                .when()
+                .get("https://restful-booker.herokuapp.com/booking")
+                .then().statusCode(200)
+                .log().all();
+    }
+
+    @DataProvider(name = "dataProvider")
+    public Object[][] dataProvider(){
+        return new Object[][]{
+                {createBookingID}
+        };
+    }
+
+    @Test(priority=3,dataProvider="dataProvider")
+    public void getDetailsBooking(String id){
+        given()
+                .log().all()
+                .when()
+                .get("https://restful-booker.herokuapp.com/booking/"+id)
+                .then()
+                .log().all();
+    }
+
+    @Test(priority = 4)
+    public void updateBooking(){
+
+        String updateRequestBody ="{\n" +
+                "    \"firstname\" : \"Jamessss\",\n" +
+                "    \"lastname\" : \"Brownnnn\",\n" +
+                "    \"totalprice\" : 111,\n" +
+                "    \"depositpaid\" : true,\n" +
+                "    \"bookingdates\" : {\n" +
+                "        \"checkin\" : \"2018-01-01\",\n" +
+                "        \"checkout\" : \"2019-01-01\"\n" +
+                "    },\n" +
+                "    \"additionalneeds\" : \"Breakfast\"\n" +
+                "}";
+
+        given()
+                .header("Content-Type","application/json")
+                .header("Cookie","token="+token)
+                .log().all()
+                .body(updateRequestBody)
+                .when()
+                .put("https://restful-booker.herokuapp.com/booking/"+createBookingID)
+                .then().statusCode(200)
+                .log().all();
+    }
+
+    @Test(priority=5)
+    public void partialUpdateBooking(){
+        Map<String,Object> queryParams= new HashMap<>();
+        queryParams.put("firstname","Jamesiii");
+        queryParams.put("lastname","Brownsss");
+
+        given()
+                .log().all()
+                .header("Content-Type","application/json")
+                .header("Cookie","token="+token)
+                .body(queryParams)
+                .when()
+                .patch("https://restful-booker.herokuapp.com/booking/"+createBookingID)
+                .then()
+                .statusCode(200)
+                .log().all();
+    }
+
+    //@AfterClass
+    @Test(priority = 6)
+    public void deleteBooking(){
+        given()
+                .log().all()
+                .header("Content-Type","Application/json")
+                .header("Cookie","token="+token)
+                .when()
+                .delete("https://restful-booker.herokuapp.com/booking/"+createBookingID)
+                .then().statusCode(201)
+                .log().all();
+    }
+}
